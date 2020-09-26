c177784c3b05dfd95e5ed6aee101b36ae61d13a8
edvinhellsing <hello@edvinhellsing.com>
2020-09-26 10:52:17 +0200

    second commit

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class BodyMassIndex {
    public static void main(String[] args) {
-      
+      double heightInput;
+      double weightInput;
+
+      try (Scanner keyboard = new Scanner(System.in); ) {
+         System.out.print("Enter your height in inches: ");
+         heightInput = keyboard.nextInt();
+
+         System.out.print("Enter your weight in pounds: ");
+         weightInput = keyboard.nextInt();
+
+         double heightInputE2 = (heightInput * heightInput);
+         double BMI = (weightInput * 703) / heightInputE2;
+
+         if (BMI > 18.5 && BMI < 25.0) {
+            System.out.print ("You have optimal weight.");
+         }
+         else if (BMI < 18.5) {
+            System.out.print ("You are considered underweight.");
+         }
+         else {
+            System.out.print ("You are considered overweight.");
+         }
+      }
    }
 }
