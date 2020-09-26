0f5bdc19be6e31a86d3db313b832f17e2a41cb5d
edvinhellsing <hello@edvinhellsing.com>
2020-09-26 12:46:28 +0200

    Changed square method of heightInput to Math.pow

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -12,7 +12,7 @@ public class BodyMassIndex {
          System.out.print("Enter your weight in pounds: ");
          weightInput = keyboard.nextInt();
 
-         double heightInputE2 = (heightInput * heightInput);
+         double heightInputE2 = Math.pow(heightInput, 2);
          double BMI = (weightInput * 703) / heightInputE2;
 
          if (BMI > 18.5 && BMI < 25.0) {
