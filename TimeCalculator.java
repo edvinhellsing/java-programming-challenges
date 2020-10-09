8138daea45ad8387517cc5452e4e01d6aab163b8
edvinhellsing <hello@edvinhellsing.com>
2020-10-09 07:38:37 +0200

    Programming challenge number 6, Time Calculator

---
+++ TimeCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TimeCalculator {
+   public static void main( String[] args) {
+      
+      int numberInput;
+
+      try (Scanner keyboard = new Scanner(System.in); ) {
+         System.out.print("Enter a number of seconds: ");
+         numberInput = keyboard.nextInt();
+
+         if (numberInput >= 86400) {
+            System.out.print("There is " + (numberInput / 86400) + " days in " + numberInput + " seconds.");
+         }
+         else if (numberInput >= 3600) {
+            System.out.print("There is " + (numberInput / 3600) + " hours in " + numberInput + " seconds.");
+         }
+         else if (numberInput >= 60) {
+            System.out.print("There is " + (numberInput / 60) + " minutes in " + numberInput + " seconds.");
+         }
+         else {
+            System.out.print("That is " + numberInput + " seconds.");
+         }
+      }
+   }
+}
